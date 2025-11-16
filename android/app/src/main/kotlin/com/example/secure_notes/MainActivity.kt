1bddab56b19fedba11410b427792aae99bd069b2
gonyango <grantononyango@gmail.com>
2025-11-16 14:50:48 +0300

    changed flutter activity to flutter fragment activity requiremnet

--- android/app/src/main/kotlin/com/example/secure_notes/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/secure_notes/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.secure_notes
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+}
